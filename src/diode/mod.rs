d4deba8c658833c89f8d7d2c063cd9d45b7343fb
sigurd4 <sigurd-s@online.no>
2025-05-01 16:06:19 +0200

    Remove unused

--- src/diode/mod.rs
+++ src/diode/mod.rs
@@ -1,8 +1,3 @@
-use core::marker::PhantomData;
-
-use num::Float;
-use crate::f;
-
 moddef::moddef!(
     flat(pub) mod {
         diode_clipper,
--- src/jfet/jfets.rs
+++ src/jfet/jfets.rs
@@ -5,7 +5,5 @@ pub struct JFET2N5458;
 impl JFETModel for JFET2N5458
 {
     const BETA: f64 = 488.9e-6;
-    const LAMBDA: f64 = 3.167e-3;
-    const LAMBDA1: f64 = Self::VTO;
     const VTO: f64 = -2.882;
 }
